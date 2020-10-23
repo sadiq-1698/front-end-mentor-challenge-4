8ff7e23d745676e517ee6a7071ef529a7cd98d0f
sadiq-1698 <mohammedsadiq1698@gmail.com>
2020-10-23 17:55:17 +0530

    Fixed image size issue

--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -6,6 +6,8 @@ const DetailsPage = () => {
 
     const { darkMode } = useContext(ThemeToggle);
 
+    const Background = "https://restcountries.eu/data/afg.svg"
+
     return (
         <div className="details-page">
             <div className={darkMode ? "back-btn dark" : "back-btn"}>
@@ -14,31 +16,35 @@ const DetailsPage = () => {
             </div>
             {/* bbbbbbbbbbbbbbbbbbbbbooooooooooooooooooooooooommmmmmmmmmmmmmmmm */}
             <div className={darkMode ? "details-container dark" : "details-container"}>
-                <img src="https://restcountries.eu/data/afg.svg" alt="flag" width="100%" height="220px"/>
+                <div className="image-container" style={{backgroundImage: `url(${Background})`}}>
+                    {/* <img src="https://restcountries.eu/data/afg.svg" alt="flag"/> */}
+                </div>
                 <div className="right-section">
-                    <div className="first">
-                        <h1>Country</h1>
-                        <div className="more-details">
-                            <h4 >Native Name:<span>&nbsp;Jublaka</span></h4>
-                            <h4 >Population:<span>&nbsp;Jinglees</span></h4>
-                            <h4 >Region:<span>&nbsp;Manglees</span></h4>
-                            <h4 >Sub Region:<span>&nbsp;Jinglees</span></h4>
-                            <h4 >Capital:<span>&nbsp;Manglees</span></h4>
+                    <div className="top">
+                        <div className="first">
+                            <h1>Country</h1>
+                            <div className="more-details">
+                                <h4 >Native Name:<span>&nbsp;Jublaka</span></h4>
+                                <h4 >Population:<span>&nbsp;Jinglees</span></h4>
+                                <h4 >Region:<span>&nbsp;Manglees</span></h4>
+                                <h4 >Sub Region:<span>&nbsp;Jinglees</span></h4>
+                                <h4 >Capital:<span>&nbsp;Manglees</span></h4>
+                            </div>
                         </div>
-                    </div>
-                    <div className="second">
-                        <h4 >Top Level Domain:<span>&nbsp;Jublaka</span></h4>
-                        <h4 >Currencies:<span>&nbsp;Jinglees</span></h4>
-                        <h4 >Languages:<span>&nbsp;Manglees</span></h4>
-                    </div>
+                        <div className="second">
+                            <h4 >Top Level Domain:<span>&nbsp;Jublaka</span></h4>
+                            <h4 >Currencies:<span>&nbsp;Jinglees</span></h4>
+                            <h4 >Languages:<span>&nbsp;Manglees</span></h4>
+                        </div>
+                    </div>   
                     <div className="third">
-                        <h4>Border Countries:&nbsp;</h4>
+                        <h4>Border Countries:&nbsp;&nbsp;</h4>
                         <div className="third-right">
                             <span>One</span>
                             <span>Two</span>
                             <span>Three</span>
                         </div>
-                    </div>
+                    </div> 
                 </div>
             </div>
         </div>
